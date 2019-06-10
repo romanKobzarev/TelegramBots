d33ece306d99cc29fd12985a177a429ceef9cb0e
romanKobzarev <romalora196@gmail.com>
2019-06-10 14:29:42 +0300

    fresh updates

--- TelegramWeather/TelegramWeather.py
+++ TelegramWeather/TelegramWeather.py
@@ -5,10 +5,15 @@ place = input("Какой город?:").strip()
 try:
     observation = owm.weather_at_place(place)
     w = observation.get_weather()
-    print(w)
 except:
     print("""
     Города "{}" не существует
     Попробуйте другой город
-    """ .format(place))
-
+    """.format(place))
+print("""В городе {City} сейчас {Status}""".format(City=place, Status=w.get_detailed_status()))
+print('''Температура сейчас равна {}'''.format(w.get_temperature('celsius')['temp']))
+if w.get_temperature('celsius')['temp'] <= 10:
+    print('Сейчас очень холодно. Надень шапку!')
+elif w.get_temperature('celsius')['temp'] <= 20:
+    print('Сейчас прохладно, лучше надеть куртку.')
+else: print('Сейчас на улице тепло. Можешь бегать в шортиках и футболке :)')
